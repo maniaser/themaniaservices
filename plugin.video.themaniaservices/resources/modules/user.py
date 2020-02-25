de65102949a2a47ea1b0cb90f4f6f3d758c6a2f6
maniaser <36471111+maniaser@users.noreply.github.com>
2020-02-25 09:41:49 +0000

    themaniaservices

---
+++ plugin.video.themaniaservices/resources/modules/user.py
@@ -0,0 +1,10 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLnRoZW1hbmlhc2VydmljZXM=')
+
+name = b.b64decode('VGhlIE1hbmlhIFNlcnZpY2Vz')
+
+host = b.b64decode('aHR0cDovL21hbmlhLnNlcnZpY2Vz')
+
+#Currently set to 8080
+port = b.b64decode('MjU0NjE=')
